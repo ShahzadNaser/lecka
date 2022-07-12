1fe30d2f88d629f5e5f5a94cdad0b2a0a3188e1c
Shahzad Naser <shahzadnaser1122@gmail.com>
2022-07-12 08:04:33 +0000

    update code

--- lecka/events/sales_order.py
+++ lecka/events/sales_order.py
@@ -10,6 +10,6 @@ def on_submit(doc, method):
                 sales_order = item.get("against_sales_order")
                 break
         if sales_order and (doc.get("shipment_id") or doc.get("carrier")):
-            frappe.db.set_value('Sales Order', sales_order, dict({'shipment_id':doc.get("shipment_id"),"carrier":doc.get("shipment_id")}))
+            frappe.db.set_value('Sales Order', sales_order, dict({'shipment_id':doc.get("shipment_id"),"carrier":doc.get("carrier")}))
             frappe.db.commit()
 
